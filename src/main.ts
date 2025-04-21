5db6fc1a533dc53bbc4a8a67e1b226366355a7b1
PcrPz <pacharaeiamsongkram@email.com>
2025-04-21 21:27:11 +0700

    init project

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // ลบ properties ที่ไม่ได้ระบุใน DTO ออก
+      forbidNonWhitelisted: true, // ไม่ยอมรับ properties ที่ไม่ได้ระบุใน DTO
+      transform: true, // แปลงค่าเป็น type ที่ถูกต้องตาม DTO
+    }),
+  );
+  
+  app.enableCors(); // เปิดใช้งาน CORS
+  
+  await app.listen(3000);
+}
+bootstrap();
