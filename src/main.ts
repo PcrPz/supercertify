01264a4786290c2e629824f1e523e342e61d38b0
PcrPz <pacharaeiamsongkram@email.com>
2025-05-01 23:35:18 +0700

    chore: enableCors*

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,10 @@ async function bootstrap() {
     }),
   );
   
-  app.enableCors({origin: 'http://localhost:3001',credentials: true}); // เปิดใช้งาน CORS
+  app.enableCors({
+    origin: '*',
+    credentials: true
+  }); // เปิดใช้งาน CORS
   
   await app.listen(3000);
 }
