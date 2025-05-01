6203bb80f751cd6fee710fd6fd72fb63bf6a5c93
PcrPz <pacharaeiamsongkram@email.com>
2025-05-02 00:10:06 +0700

    fix: enableCor2

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   );
   
   app.enableCors({
-    origin:["http://localhost:3001/","https://supercertify-front.vercel.app/"],
+    origin:["http://localhost:3001","https://supercertify-front.vercel.app"],
     credentials: true,
   }); // เปิดใช้งาน CORS
   
