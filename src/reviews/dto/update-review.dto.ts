60776c7cb1af44e3edd614d07c754f2aefaa98dd
PcrPz <pacharaeiamsongkram@email.com>
2025-06-06 17:08:10 +0700

    feat:Update Dto

---
+++ src/reviews/dto/update-review.dto.ts
@@ -0,0 +1,35 @@
+import { IsOptional, IsNumber, IsString, Min, Max, IsBoolean, IsArray, IsObject } from 'class-validator';
+
+export class UpdateReviewDto {
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  @Max(5)
+  rating?: number;
+
+  @IsOptional()
+  @IsString()
+  comment?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isPublic?: boolean;
+
+  @IsOptional()
+  @IsArray()
+  tags?: string[];
+}
+
+export class AdminUpdateReviewDto {
+  @IsOptional()
+  @IsBoolean()
+  isVerified?: boolean;
+
+  @IsOptional()
+  @IsString()
+  adminResponse?: string;
+  
+  @IsOptional()
+  @IsBoolean()
+  isDisplayed?: boolean;
+}
